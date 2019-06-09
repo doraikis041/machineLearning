21866646dc20d7433a176a68d642fd52cf94bbf9
letsuarez83 <letsuarez83@gmail.com>
2019-06-09 16:50:25 -0300

    falta agregar las variables correctas. Solo puse las primeras 10 porque son las numericas. falta pasar de factor a numerico el resto

--- KNN.R
+++ KNN.R
@@ -13,3 +13,31 @@ h.train <- h.part$train
 h.test <- h.part$test
 
 #Entrenamiento del algoritmo
+
+
+h.test$Churn <- ifelse(h.test$Churn == "Yes", 1, 0)
+h.train$Churn <- ifelse(h.train$Churn == "Yes", 1, 0)
+
+h.test1 <- h.test[,1:10]
+h.train1 <- h.train[,1:10]
+
+h.k <- 10
+h.A <- function(k, train, test) {knn(train, test, cl = train$Churn, k)}
+h.t_predTest1 <- list()
+for (k in seq(h.k)) {
+  h.t_predTest1[[k]] <- h.A(k, h.train1, h.test1)
+  }
+
+print(h.t_predTest1)
+
+
+# error de knn
+
+h.error_test <- list()
+for (k in seq(h.k)) {
+  errorsTest <- fn_err_cla(h.t_predTest1[[k]],h.test1$Churn)
+  h.error_test[[k]] <- mean(as.vector(unlist(errorsTest)))
+}
+
+print(h.error_test)
+
