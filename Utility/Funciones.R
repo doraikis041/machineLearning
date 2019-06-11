2f4746888fec943392c1e20add9fd551cd4d9e3e
Doris Medina <4051@DivinoSA.local>
2019-06-10 22:34:44 -0300

    Se modifica las funciones de rparth

--- Utility/Funciones.R
+++ Utility/Funciones.R
@@ -57,14 +57,7 @@ naiveBayes_fit_formulas <- function(train, formulas) {
 ###########################################################
 ########       Knn       #######################3##########
 ###########################################################
-# knn_fit_formulas <- function(train,test, formulas) {
-#   list_fit <- list()
-#   for (i in seq(1, length(formulas))) {
-#     list_fit[[i]] <- knn(data = train,test = test, cl = train$y, as.formula(formulas[i]), )
-#   }
-#   return(list_fit)
-# }
-# #h.A <- function(k, train, test) {knn(train, test, cl = train$y, k)}
+
 
 
 # Prediccion & error Naive Bayes
@@ -98,56 +91,58 @@ cv_err_nBayes <- function(cv_part, formulas, y) {
 
 ## Se agregar todo lo de arbol
 #######################################################################
-rpart_fit_formulas <- function(train, formulas, method = 'class') {
+rpart_fit_formulas <- function(train, formulas, method = 'class',control, parametros) {
   list_fit <- list()
   for (i in seq(1, length(formulas))) {
-    list_fit[[i]] <- rpart(as.formula(formulas[i]), 
-                           data = train, 
-                           method = method)
+    list_fit[[i]] <- rpart(as.formula(formulas[i]),data = train,control = control,method = method, parms = parametros )
   }
   return(list_fit)
 }
 
 # Clasificacion con rpart para una lista de cp
-
-rpart_fit_ctrl <- function(train, formula, ctrl, method = 'class', hparms) {
+rpart_fit_ctrl <- function(train, formulas, ctrl, method = 'class', parametros) {
   list_fit <- list()
   for (i in seq(1, length(ctrl))) {
-    list_fit[[i]] <- rpart(as.formula(formula), 
-                           data = train, 
-                           control = ctrl[[i]],
-                           method = method,
-                           parms = hparms)
+    list_fit[[i]] <- rpart_fit_formulas(formulas = formulas,
+                                        train = train,
+                                        control = ctrl[[i]],
+                                        method = method,
+                                        parametros = parametros)
   }
   return(list_fit)
 }
 
 # Plot de lista de arboles
-
 rpart_plot_fit <- function(list_tree) {
-  for (i in seq(1, length(list_tree))) {
-    rpart.plot(list_tree[[i]], roundint = FALSE)
+  for (i in seq(1, length(list_tree))){
+    for (j in length(list_tree[[i]])) {
+      rpart.plot(list_tree[[i]][[j]], roundint = FALSE)
+    }
   }
 }
 
 
 # Prediccion y error de clasificacion con rpart
-
 rpart_pred_err <- function(list_fit, newdata, y) {
   test_pred <- list()
-  test_err <- rep(0, length(list_fit))
+  #test_err <- rep(0, length(list_fit))
+  test_err <- list()
   for (i in seq(1, length(list_fit))) {
-    # prediccion
-    test_pred[[i]] <- predict(list_fit[[i]], newdata = newdata, type = 'class')
-    # error de clasificacion
-    test_err[i] <- fn_err_cla(test_pred[[i]], newdata[[y]])
-  }
-  list(pred = test_pred, err = test_err)
+    test_pred[[i]] <- list()
+    test_err[[i]] <- list()
+    for(j in seq(1, length(list_fit[[i]]))){
+      # prediccion
+      test_pred[[i]][[j]] <- predict(list_fit[[i]][[j]], newdata = newdata, type = 'class')
+      # error de clasificacion
+      #head(list_fit[[i]][[j]],2)
+    test_err[[i]][[j]]<- fn_err_cla(test_pred[[i]][[j]], newdata[[y]])
+    }
+    }
+  list(pred = test_pred, err = test_err )
 }
 
 
 # Prediccion y error de cv con rpart
-
 rpart_cv_err <- function(cv_part, formula, ctrl, y) {
   cv_test <- cv_part$test
   cv_train <- cv_part$train
