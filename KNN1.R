7782dbf6fa406042620c886b41ad7f2f80a4dbfb
letsuarez83 <letsuarez83@gmail.com>
2019-06-12 21:25:53 -0300

    se actualiza la funcion de error y knn. falta hacer el cross validation

--- KNN1.R
+++ KNN1.R
@@ -1,48 +1,28 @@
-source("Utility/head.R")
-source("Utility/funciones.R")
 
-#Genero la grilla de visualización
-h.gridDF <- dfGridMetada()
 
-#Se carga los datos con las ETL generales
-h.data <- loadData()
+source("Utility/functions.R")
 
-#Generar la particion de test & train
-h.part <- partition_train_test(h.data, ntrain = nTrain)
-h.train <- h.part$train
-h.test <- h.part$test
 
-#Entrenamiento del algoritmo
+# Cargar datos
 
-h.numeric_vars <- which(sapply(h.data, is.numeric))
-h.train <- h.train[,h.numeric_vars]
-h.test <- h.test[,h.numeric_vars]
+raw_data <- loadData()
+
+h.data <- partition_train_test_numeric (raw_data, nTrain)
+h.train <- h.data$train
+h.test <- h.data$test
 
 h.train1 <- h.train[,-h.train$Churn]
 h.test1 <- h.test[,-h.test$Churn]
 
 h.k <- 2
 h.A <- function(k, train, test, y) {knn(train, test, cl = as.factor(y), k)}
-h.t_predTest1 <- list()
+h.t_predTest <- list()
 for (k in seq(h.k)) {
-  h.t_predTest1[[k]] <- h.A(k, h.train1, h.test1, y = h.train$Churn)
+  h.t_predTest[[k]] <- h.A(k, h.train1, h.test1, y = h.train$Churn)
   }
 
-print(h.t_predTest1)
+print(h.t_predTest)
 
-
-
-
-
-
-# h.t_predTrain <- list()
-# for (k in seq(h.k)) {
-#   h.t_predTrain[[k]] <- list()
-#   for (s in seq(h.ds_t)) {
-#     h.t_predTest[[k]][[s]] <- h.A(k,h.T[[s]], T0)
-#     h.t_predTrain[[k]][[s]] <- h.A(k,h.T[[s]], h.T[[s]])
-#   }
-# }
 
 
 
@@ -50,7 +30,7 @@ print(h.t_predTest1)
 
 h.error_test <- list()
 for (k in seq(h.k)) {
-  errorsTest <- fn_err_cla(h.t_predTest1[[k]],h.test$Churn)
+  errorsTest <- fn_err_cla(h.t_predTest[[k]],h.test$Churn)
   h.error_test[[k]] <- mean(as.vector(unlist(errorsTest)))
 }
 
