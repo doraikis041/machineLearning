35f1c34fa0f5996178f954d71b4a660c6a8d6a68
Doris Medina <4051@DivinoSA.local>
2019-06-12 23:55:37 -0300

    Se agregar matriz de confusión para rpart

--- Utility/functions.R
+++ Utility/functions.R
@@ -15,10 +15,10 @@ set.seed(117)
 #Variables Generales
 nTrain <- 37445
 
-### Grilla para visualizacion
+# Grilla para visualizacion
 dfGridMetada <- function(min = 0, max = 5, by = 0.1){grid_df <- data.frame(x = seq(min, max, by))}
 
-# Carga de los datos principales con las ETL generales para todos los algoritmos
+## Carga de los datos principales con las ETL generales para todos los algoritmos
 loadData <- function()
 {
   T0 <- read.csv(file = "Dataset/dataset.csv")
@@ -28,22 +28,17 @@ loadData <- function()
   df <- na.omit(T0)
 }
 
-#Defincion de funciones utilizadas en los direfentes algoritmos
-
-###########################################################
-########### Error de clasificacion  #######################
-###########################################################
+## Calculo del Error de clasificación
 fn_err_cla <- function(yhat, y) { mean(yhat != y) }
 
 
-###########################################################
-###########  Particion train-test_Numeric and other #######
-###########################################################
+## Creación de la particion train-test general
 partition_train_test <- function(df, ntrain = 10) {
   train_idx <- sample.int(nrow(df), size = ntrain)
   list(train = df[train_idx,], test = df[-train_idx,])
 }
 
+## Creación de la particion train-test solo numeric
 partition_train_test_numeric <- function(df, ntrain = 10) {
   df_numeric <- which(sapply(df,is.numeric))
   df <- df[,df_numeric]
@@ -52,9 +47,7 @@ partition_train_test_numeric <- function(df, ntrain = 10) {
 }
 
 
-###########################################################
-###########   Particion en 5 folds  #######################
-###########################################################
+## Particion en K folds, por defecto k_folds = 5
 partition_cv <- function(df, k_folds = 5) {
   cv_test <- split(df, seq(1, k_folds))
   cv_train <- list()
@@ -67,8 +60,9 @@ partition_cv <- function(df, k_folds = 5) {
   list(train = cv_train, test = cv_test, k_folds = k_folds)
 }
 
+
 ###########################################################
-## Regresion con glm para una lista de formulas  ##########
+###########        Regresion con glm             ##########
 ###########################################################
 glm_fit_formulas_cla <- function(train, formulas) {
   list_fit <- list()
@@ -123,14 +117,7 @@ glm_pred_err_mc <- function(list_fit, newdata, y) {
   list(prob = test_prob, pred = test_pred, err = test_err, mc = test_mc)
   }
 
-
-
-
-
-
-
 # Prediccion y error MSE
-
 cv_err <- function(cv_part, formulas, y) {
   cv_test <- cv_part$test
   cv_train <- cv_part$train
@@ -157,13 +144,6 @@ naiveBayes_fit_formulas <- function(train, formulas) {
   }
   return(list_fit)
 }
-
-
-###########################################################
-########       Knn       #######################3##########
-###########################################################
-
-
 
 # Prediccion & error Naive Bayes
 nbayes_pred_err <- function(list_fit, newdata, y) {
@@ -194,56 +174,39 @@ cv_err_nBayes <- function(cv_part, formulas, y) {
 }
 
 
-## Se agregar todo lo de arbol
-#######################################################################
-rpart_fit_formulas <- function(train, formulas, method = 'class',control, parametros) {
-  list_fit <- list()
-  for (i in seq(1, length(formulas))) {
-    list_fit[[i]] <- rpart(as.formula(formulas[i]),data = train,control = control,method = method, parms = parametros )
-  }
-  return(list_fit)
-}
 
-# Clasificacion con rpart para una lista de cp
-rpart_fit_ctrl <- function(train, formulas, ctrl, method = 'class', parametros) {
+#######################################################################
+#################### Arboles con rpart ################################
+#######################################################################
+
+
+### FIT con rpart para una lista de CP
+rpart_fit_ctrl <- function(train, formula, ctrl, method = 'class', parametros) {
   list_fit <- list()
   for (i in seq(1, length(ctrl))) {
-    list_fit[[i]] <- rpart_fit_formulas(formulas = formulas,
-                                        train = train,
-                                        control = ctrl[[i]],
-                                        method = method,
-                                        parametros = parametros)
+    list_fit[[i]] <- rpart(as.formula(formula),data = train,control = ctrl[[i]],method = method, parms = parametros ) 
   }
   return(list_fit)
 }
 
-# Plot de lista de arboles
+## Plot de lista de arboles
 rpart_plot_fit <- function(list_tree) {
   for (i in seq(1, length(list_tree))){
-    for (j in length(list_tree[[i]])) {
-      rpart.plot(list_tree[[i]][[j]], roundint = FALSE)
+      rpart.plot(list_tree[[i]], roundint = FALSE)
     }
-  }
 }
 
-
-# Prediccion y error de clasificacion con rpart
+## Prediccion y error de clasificacion con rpart
 rpart_pred_err <- function(list_fit, newdata, y) {
   test_pred <- list()
-  #test_err <- rep(0, length(list_fit))
-  test_err <- list()
+  test_err <- rep(0, length(list_fit))
   for (i in seq(1, length(list_fit))) {
-    test_pred[[i]] <- list()
-    test_err[[i]] <- list()
-    for(j in seq(1, length(list_fit[[i]]))){
-      # prediccion
-      test_pred[[i]][[j]] <- predict(list_fit[[i]][[j]], newdata = newdata, type = 'class')
-      # error de clasificacion
-      #head(list_fit[[i]][[j]],2)
-    test_err[[i]][[j]]<- fn_err_cla(test_pred[[i]][[j]], newdata[[y]])
-    }
-    }
-  list(pred = test_pred, err = test_err )
+    # prediccion
+    test_pred[[i]] <- predict(list_fit[[i]], newdata = newdata, type = 'class')
+    # error de clasificacion
+    test_err[i] <- fn_err_cla(test_pred[[i]], newdata[[y]])
+  }
+  list(pred = test_pred, err = test_err)
 }
 
 
@@ -262,3 +225,27 @@ rpart_cv_err <- function(cv_part, formula, ctrl, y) {
 }
 
 
+## Matrix de confusión con lista de umbrales (Reutilizando glm_pred_err_mc)
+rpart_pred_err_mc <- function(list_fit, newdata, y,type = 'class',umbral) {
+  test_prob <- list()
+  test_pred <- list()
+  test_err  <- list ()
+  test_mc <- list()
+  for (i in seq(1, length(list_fit))) {
+    # probabilidad
+    test_prob[[i]] <- predict(list_fit[[i]], newdata = newdata, type = 'class') #para que de la probabilidad
+    # prediccion 
+    test_err[[i]] <- rep(0, length(umbral))
+    test_pred[[i]] <- list ()
+    test_mc[[i]] <- list ()
+    for (j in seq(1, length(umbral))) {
+      # prediccion 
+      test_pred[[i]][[j]] <- ifelse(as.numeric(test_prob[[i]]) > umbral[j],1,0)
+      # error 
+      test_err[[i]][j] <- fn_err_cla(test_pred[[i]][[j]], newdata[[y]])
+      # mc 
+      test_mc[[i]][[j]] <- table(test_pred[[i]][[j]],newdata[[y]])
+    }
+  }
+  list(prob = test_prob, pred = test_pred, err = test_err, mc = test_mc)
+}
