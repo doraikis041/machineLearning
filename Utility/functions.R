7e50507046b1a87b60b5c398f65660676f883862
Doris Medina <4051@DivinoSA.local>
2019-06-17 22:29:50 -0300

    Se agrega el codigo de boosting del profe

--- Utility/functions.R
+++ Utility/functions.R
@@ -33,6 +33,17 @@ loadData <- function()
 ## Calculo del Error de clasificación
 fn_err_cla <- function(yhat, y) { mean(yhat != y) }
 
+# Funcion de costo
+fn_err_cost <- function(yhat, y) { 
+  fp_cost <- 10 * sum( yhat == 1 & y == 0 )
+  tp_cost <- -25.5 * sum( yhat == 1 & y == 1 )
+  return((fp_cost + tp_cost) / length(y)) 
+}
+
+
+##Funcion de error
+#fn_err <- fn_err_cla 
+fn_err <- fn_err_cost 
 
 ## Creación de la particion train-test general
 partition_train_test <- function(df, ntrain = 10) {
@@ -87,7 +98,7 @@ glm_pred_err_cla <- function(list_fit, newdata, y, umbral =0.5) {
     # prediccion
     test_pred[[i]] <- ifelse(test_prob[[i]] > umbral,1,0)
     # error de clasificacion
-    test_err[i] <- fn_err_cla(test_pred[[i]], newdata[[y]])
+    test_err[i] <- fn_err(test_pred[[i]], newdata[[y]])
   }
   list(prob = test_prob, pred = test_pred, err = test_err)
 }
@@ -111,7 +122,7 @@ glm_pred_err_mc <- function(list_fit, newdata, y) {
     # prediccion 
     test_pred[[i]][[j]] <- ifelse(test_prob[[i]] > umbral[j],1,0)
     # error 
-    test_err[[i]][j] <- fn_err_cla(test_pred[[i]][[j]], newdata[[y]])
+    test_err[[i]][j] <- fn_err(test_pred[[i]][[j]], newdata[[y]])
     # mc 
     test_mc[[i]][[j]] <- table(test_pred[[i]][[j]],newdata[[y]])
           }
@@ -155,7 +166,7 @@ nbayes_pred_err <- function(list_fit, newdata, y) {
     # prediccion
     test_pred[[i]] <- predict(list_fit[[i]], newdata = newdata)
     # Error clasificacion
-    test_err[i] <- fn_err_cla(test_pred[[i]], newdata[[y]])
+    test_err[i] <- fn_err(test_pred[[i]], newdata[[y]])
   }
   list(pred = test_pred, err = test_err)
   
@@ -204,7 +215,7 @@ rpart_pred_err <- function(list_fit, newdata, y) {
     # prediccion
     test_pred[[i]] <- predict(list_fit[[i]], newdata = newdata, type = 'class')
     # error de clasificacion
-    test_err[i] <- fn_err_cla(test_pred[[i]], newdata[[y]])
+    test_err[i] <- fn_err(test_pred[[i]], newdata[[y]])
   }
   list(pred = test_pred, err = test_err)
 }
@@ -242,7 +253,7 @@ rpart_pred_err_mc <- function(list_fit, newdata, y,type = 'class',umbral) {
       # prediccion 
       test_pred[[i]][[j]] <- ifelse(as.numeric(test_prob[[i]]) > umbral[j],1,0)
       # error 
-      test_err[[i]][j] <- fn_err_cla(test_pred[[i]][[j]], newdata[[y]])
+      test_err[[i]][j] <- fn_err(test_pred[[i]][[j]], newdata[[y]])
       # mc 
       test_mc[[i]][[j]] <- table(test_pred[[i]][[j]],newdata[[y]])
     }
@@ -276,7 +287,7 @@ randomForest_pred_err <- function(list_fit, newdata, y) {
                               newdata = newdata,
                               type = 'class') #class automatically converted to "response", for backward compatibility
     #Clasification error
-    test_err[i] <- fn_err_cla(test_pred[[i]], newdata[[y]])
+    test_err[i] <- fn_err(test_pred[[i]], newdata[[y]])
   }
   list(pred = test_pred, err = test_err)
 }
@@ -284,7 +295,7 @@ randomForest_pred_err <- function(list_fit, newdata, y) {
 randomForest_cv_err <- function(cv_part, formula, y, pNtree, pMtry) {
   cv_test <- cv_part$test
   cv_train <- cv_part$train
-  cv_matrix_err <- matrix(0, nrow = cv_part$k_folds, ncol = length(ctrl))
+  cv_matrix_err <- matrix(0, nrow = cv_part$k_folds)
   for (k in seq(1, cv_part$k_folds)) {
     #randomForest
     list_fit <- randomForest_fit(formula = as.formula(formula),
@@ -296,24 +307,117 @@ randomForest_cv_err <- function(cv_part, formula, y, pNtree, pMtry) {
     cv_matrix_err[k, ] <- list_pred_err$err  
   }
   apply(cv_matrix_err, 2, mean)
+}
 
   #######################################################################
   ####################   Boosting        ################################
   #######################################################################
   
-  # Fit using Boosting (gbm)
-  boosting_fit <- function(pTrain, pFormula, dDistribution = "gaussian", pControl) {
-    list_fit <- list()
-    for (i in seq(1, length(pControl))) {
-      list_fit[[i]] <- gbm(pFormula,
-                           data = pTrain,
-                           distribution = dDistribution,
-                           interaction.depth = pControl[[i]]$interaction.depth,
-                           shrinkage = pControl[[i]]$shrinkage,
-                           n.trees = pControl[[i]]$n.trees)
-      }
-      return(list_fit)
+  # # Fit using Boosting (gbm)
+  # boosting_fit <- function(pTrain, pFormula, dDistribution = "bernoulli", pControl) {
+  #   list_fit <- list()
+  #   for (i in seq(1, length(pControl))) {
+  #     list_fit[[i]] <- gbm(pFormula,
+  #                          data = pTrain,
+  #                          distribution = dDistribution,
+  #                          interaction.depth = pControl[[i]]$interaction.depth,
+  #                          shrinkage = pControl[[i]]$shrinkage,
+  #                          n.trees = pControl[[i]]$n.trees)
+  #     }
+  #     return(list_fit)
+  #   }
+  # }
+
+
+gbm_fit_ctrl <- function(train, formula, ctrl) {
+  # train: datos de entrenamiento
+  # formula: una formula (para clasificacion usar as.factor en la variable a predecir)
+  # ctrl: es una lista de listas cada lista en ctrl tiene dos atributos: ntree y mtry
+  list_fit <- list()
+  for (i in seq(1, length(ctrl))) {
+    list_fit[[i]] <- gbm(formula = as.formula(formula), 
+                         data = train, 
+                         distribution = 'bernoulli',
+                         n.trees = ctrl[[i]]$ntree,
+                         interaction.depth = ctrl[[i]]$depth,
+                         shrinkage = ctrl[[i]]$shrinkage)
+  }
+  return(list_fit)
+}
+
+# Prediccion
+
+gbm_prob <- function(list_fit, newdata, ctrl) {
+  # list_fit: lista retornada por gbm_fit_ctrl
+  # newdata: datos de test
+  test_prob <- list()
+  for (i in seq(1, length(list_fit))) {
+    # prediccion
+    test_prob[[i]] <- predict(list_fit[[i]], 
+                              newdata = newdata, 
+                              n.trees = ctrl[[i]]$ntree,
+                              type = 'response')
+  }
+  return(test_prob)
+}
+
+# Prediccion para un vector de umbrales
+gbm_pred <- function(test_prob, umbral) {
+  # test_prob: lista retornada por gbm_prob
+  # umbral: vector de umbrales entre 0 y 1
+  test_pred <- list()
+  for (i in seq(1, length(test_prob))) {
+    test_pred[[i]] <- list()
+    for (j in seq(1, length(umbral))) {
+      # prediccion
+      test_pred[[i]][[j]] <- ifelse(test_prob[[i]] > umbral[j], 1, 0)
     }
   }
+  return(test_pred)
+}
 
+# Error de prediccion para una lista de predicciones
+
+gbm_pred_err <- function(test_pred, y) {
+  # test_pred: lista retornada por gbm_pred
+  test_pred_err <- list()
+  for (i in seq(1, length(test_pred))) {
+    test_pred_err[[i]] <- rep(0, length(test_pred[[i]]))
+    for (j in seq(1, length(test_pred[[i]]))) {
+      # error
+      test_pred_err[[i]][j] <- fn_err(test_pred[[i]][[j]], y)
+    }
+  }
+  return(test_pred_err)
+}
+
+# Error de CV
+
+gbm_cv_err <- function(cv_part, formula, ctrl, umbral = 0.5, var_y) {
+  # cv_part: particion generada por partition_cv
+  # formula: formula
+  # ctrl: lista de controles de rf
+  # umbral: umbral para calcular la prediccion
+  # y: variable a predecir
+  cv_test <- cv_part$test
+  cv_train <- cv_part$train
+  cv_err <- list()
+  for (k in seq(1, cv_part$k_folds)) {
+    # fit
+    list_fit <- gbm_fit_ctrl(cv_train[[k]], 
+                             formula = formula, 
+                             ctrl = ctrl)
+    # prob
+    list_prob <- gbm_prob(list_fit, 
+                          newdata = cv_test[[k]],
+                          ctrl = ctrl)
+    # pred
+    list_pred <- gbm_pred(list_prob, 
+                          umbral = umbral) 
+    # error
+    cv_err[[k]] <- gbm_pred_err(list_pred, 
+                                cv_test[[k]][[var_y]])
+  }
+  return(mean(unlist(cv_err)))
+}
 
